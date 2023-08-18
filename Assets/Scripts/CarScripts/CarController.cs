6a57601da4811ca055235ed39763a5b5ad89394d
Rodwin <Rodwin@Nyamakop.co.za>
2023-08-18 21:04:00 +0200

    Created Car Motor

--- Assets/Scripts/CarScripts/CarController.cs
+++ Assets/Scripts/CarScripts/CarController.cs
@@ -4,22 +4,56 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
+    Rigidbody rb;
     public WheelColliders wheelColliders;
     public WheelMeshs wheelMeshs;
+    public float throttleInput;
+    public float steeringInput;
+    public AnimationCurve steeringCurve;
+
+    public float motorPower = 500;
+    float speed;
+
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
+
     void Update()
     {
+        speed = rb.velocity.magnitude;
+        CheckInput();
+        ApplyMotorTorque();
         ApplyWheelPositions();
+        ApplySteering();
     }
+
+    void CheckInput()
+    {
+        throttleInput = Input.GetAxis("Vertical");
+        steeringInput = Input.GetAxis("Horizontal");
+    }
+
+    void ApplyMotorTorque()
+    {
+        //These are the driven wheels. This particular car is rear wheel drive but that can be easily altered by changing the wheels that recieve torque. 
+        wheelColliders.RLWheel.motorTorque = motorPower * throttleInput;
+        wheelColliders.RRWheel.motorTorque = motorPower * throttleInput;
+    }
+
     void ApplyWheelPositions()
     {
         UpdateWheelPosition(wheelColliders.FLWheel,wheelMeshs.FLWheel);
         UpdateWheelPosition(wheelColliders.FRWheel, wheelMeshs.FRWheel);
         UpdateWheelPosition(wheelColliders.RLWheel, wheelMeshs.RLWheel);
         UpdateWheelPosition(wheelColliders.RRWheel, wheelMeshs.RRWheel);
+    }
+
+    void ApplySteering()
+    {
+        float steeringAngle = steeringInput*steeringCurve.Evaluate(speed);
+        wheelColliders.FRWheel.steerAngle = steeringAngle;
+        wheelColliders.FLWheel.steerAngle = steeringAngle;
     }
     void UpdateWheelPosition(WheelCollider collider, GameObject wheelMesh)
     {
