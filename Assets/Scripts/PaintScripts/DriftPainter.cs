1b7c537378665101bd73b86a41ed2d028a53547c
Erik Prinz <erik88d3@gmail.com>
2023-10-09 21:34:35 +0200

    Improved Paint Trail Smoothness
    Removed artifacts where paintable surfaces met (mostly - can be improved further, at the slight cost of performance).

--- Assets/Scripts/PaintScripts/DriftPainter.cs
+++ Assets/Scripts/PaintScripts/DriftPainter.cs
@@ -5,10 +5,13 @@ public class DriftPainter : MonoBehaviour
 
     [Space]
 
-    [Tooltip("Where the painter raycast will fire down from, relative to car body centre.")]
+    [Tooltip("Where the central painter raycast will fire down from, relative to car body centre.")]
     [SerializeField] Vector3 offset = new Vector3(0,0,0);
     [Tooltip("How far from the ground can we paint from?")]
     [SerializeField] float paintDistance = 1f;
+    const float sideOffset = 1.5f;
+    const float frontOffset = 2.75f;
+    const float backOffset = 3f;
 
     [Space]
 
@@ -49,23 +52,89 @@ public class DriftPainter : MonoBehaviour
     {
         if (driftManager.isDrifting)  
         {
+            // Central Ray --------------------------------------------------------------------------------------
             Ray ray = new Ray((transform.position + offset), Vector3.down);
             RaycastHit hit;
+            Vector3 hitPoint = (transform.position + offset);
 
             if (Physics.Raycast(ray, out hit, paintDistance))
             {
                 //Debug.DrawRay(ray.origin, hit.point - ray.origin, Color.red);
-                Paintable p = hit.collider.GetComponent<Paintable>();                 //ERIK UPDATE THIS TO CHECK THE WHOLE CAR BOTTOM!
+                Paintable p = hit.collider.GetComponent<Paintable>();                 
                 if (p != null)
                 {
                     rotation = transform.rotation.eulerAngles.y*Mathf.Deg2Rad;
                     PaintManager.instance.paint(p, hit.point, radius, hardness, strength, width, height, rotation, paintColor);
+
+                    hitPoint = hit.point; // THIS METHOD WILL ONLY WORK WHEN THERE"S PAINTABLE GROUND UNDER CAR. if we're near paintable ground, but not on it, we'll get an artifact.
 
                     //Update Score
                     p.CalculateScore();
                     GameModeManager.instance.ScoreUpdatedThisFrame = true;
                 }
             }
+            // End Central ----------------------------------------------------------------------------------------
+
+
+            // Left Ray --------------------------------------------------------------------------------------
+            ray = new Ray((transform.position + offset + new Vector3(-sideOffset, 0, 0)), Vector3.down);
+
+            if (Physics.Raycast(ray, out hit, paintDistance))
+            {
+                //Debug.DrawRay(ray.origin, hit.point - ray.origin, Color.red);
+                Paintable p = hit.collider.GetComponent<Paintable>();
+                if (p != null)
+                {
+                    rotation = transform.rotation.eulerAngles.y * Mathf.Deg2Rad;
+                    PaintManager.instance.paint(p, hitPoint, radius, hardness, strength, width, height, rotation, paintColor);
+                }
+            }
+            // End Left ----------------------------------------------------------------------------------------
+
+            // Right Ray --------------------------------------------------------------------------------------
+            ray = new Ray((transform.position + offset + new Vector3(sideOffset, 0, 0)), Vector3.down);
+
+            if (Physics.Raycast(ray, out hit, paintDistance))
+            {
+                //Debug.DrawRay(ray.origin, hit.point - ray.origin, Color.red);
+                Paintable p = hit.collider.GetComponent<Paintable>();
+                if (p != null)
+                {
+                    rotation = transform.rotation.eulerAngles.y * Mathf.Deg2Rad;
+                    PaintManager.instance.paint(p, hitPoint, radius, hardness, strength, width, height, rotation, paintColor);
+                }
+            }
+            // End Right ----------------------------------------------------------------------------------------
+
+            // Front Ray --------------------------------------------------------------------------------------
+            ray = new Ray((transform.position + offset + new Vector3(0, 0, frontOffset)), Vector3.down);
+
+            if (Physics.Raycast(ray, out hit, paintDistance))
+            {
+                //Debug.DrawRay(ray.origin, hit.point - ray.origin, Color.red);
+                Paintable p = hit.collider.GetComponent<Paintable>();
+                if (p != null)
+                {
+                    rotation = transform.rotation.eulerAngles.y * Mathf.Deg2Rad;
+                    PaintManager.instance.paint(p, hitPoint, radius, hardness, strength, width, height, rotation, paintColor);
+                }
+            }
+            // End Front ----------------------------------------------------------------------------------------
+
+            // Back Ray --------------------------------------------------------------------------------------
+            ray = new Ray((transform.position + offset + new Vector3(0, 0, -backOffset)), Vector3.down);
+
+            if (Physics.Raycast(ray, out hit, paintDistance))
+            {
+                //Debug.DrawRay(ray.origin, hit.point - ray.origin, Color.red);
+                Paintable p = hit.collider.GetComponent<Paintable>();
+                if (p != null)
+                {
+                    rotation = transform.rotation.eulerAngles.y * Mathf.Deg2Rad;
+                    PaintManager.instance.paint(p, hitPoint, radius, hardness, strength, width, height, rotation, paintColor);
+                }
+            }
+            // End Back ----------------------------------------------------------------------------------------
 
             scoreCapsule.SetActive(true);
         }
